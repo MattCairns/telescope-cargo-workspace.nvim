f62fad782838b0c991a1be4fccef2c264d8179fa
matthew cairns <matthew@cairns.pro>
2023-05-28 18:57:15 -0700

    fix: change name to telescope-cargo-workspace

--- lua/telescope-cargo-workspace/init.lua
+++ lua/telescope-cargo-workspace/init.lua
@@ -1,6 +1,8 @@
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 
 local M = {}
 
@@ -20,6 +22,15 @@ M.pick_workspace = function(opts)
       end
     },
     sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        -- print(vim.inspect(selection))
+        vim.api.nvim_put({ selection[1] }, "", false, true)
+      end)
+      return true
+    end,
   }):find()
 end
 
